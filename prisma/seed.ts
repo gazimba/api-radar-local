2ef7a9b3a81f52a0a6d54d240a3fdd40919748a7
Gabriel Martins <gabrielb3r@gmail.com>
2026-04-12 12:10:46 -0300

    seed do prisma funcionando

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+// 👇 Importação adicionada aqui!
 import { hash } from "bcrypt";
-import "dotenv/config";
 
-const prisma = new PrismaClient();
+// 1. Criamos a conexão usando a variável de ambiente
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+// 2. Passamos para o adaptador do Postgres
+const adapter = new PrismaPg(pool);
+
+// 3. Inicializamos o cliente do Prisma 7 corretamente
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
+    // Agora o JavaScript sabe de onde vem o "hash"
     const passwordHash = await hash("admin123", 8);
 
     await prisma.user.upsert({
